ec46935ff166b87e3af4d9374564cb1125c64f7f
Christopher N. Lee <43392988+clee22@users.noreply.github.com>
2022-04-27 01:48:35 -0400

    Update create_everything.py

--- cli/create_everything.py
+++ cli/create_everything.py
@@ -1,11 +1,67 @@
 # i intend this file to load everything (e.g. tokenizer, model, datasets, etc) and then save them locally
 
+import os
+import argparse
+import logging
+from packaging import version
+
 import pandas as pd
-import datasets 
 from datasets import load_dataset, concatenate_datasets
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-# source_tokenizer.save_pretrained(os.path.join(args.save_dir, f"{args.target_lang}_tokenizer"))
+# Setup logging
+logger = logging.getLogger(__file__)
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="create everything")
+
+    parser.add_argument(
+        "--checkpoint_name",
+        type=str,
+        default="google/mt5-base",
+        help=("Which checkpoint of mt5 do you want to you"),
+    )
+    parser.add_argument("--save_dir", type=str, required=True, help="Directory which will be used to save everything.")
+
+    args = parser.parse_args()
+    return args
+    
+def main():
+  args = parse_args()
+  logger.info(f"Starting to make everything with args {args}")
+  ######################################## LANG TOKEN MAP ########################################
+  # since I dont plan on changing the datasets for this I will make the lang token map hardcoded 
+  lang_tokens = {'en': '<en>', 'hu': '<hu>', 'ru': '<ru>', 'de': '<de>', 'es': '<es>'}
+  ########################################## Tokenizer ###########################################
+  logger.info(f"Loading pretrained tokenizer")
+  tokenizer = AutoTokenizer.from_pretrained(args.checkpoint_name, use_fast=True)
+  logger.info(f"Adding language tokens to pretrained tokenizer")
+  lang_tok_map = {"additional_special_tokens" : list(lang_tokens.values())}
+  tokenizer.add_special_tokens(lang_tok_map)
+  logger.info(f"Saving pretrained tokenizer")
+  tokenizer.save_pretrained(os.path.join(args.save_dir, f"{args.checkpoint_name}_tokenizer"))
+  ######################## Base Model For Custom Model & Comparison ##############################
+  # the idea is i load it twice but save both the custom model and the comparison model
+  # by doing so i intend so that i will not ever need to run this script again to get a fresh model
+  # from re downloading the checkpoint
+  logger.info(f"Loading model")
+  base_model = AutoModelForSeq2SeqLM.from_pretrained(args.checkpoint_name)
+  logger.info(f"Resizing model embeddings")
+  base_model.resize_token_embeddings(len(tokenizer))
+  logger.info(f"Saving model")
+  torch.save(base_model, os.path.join(args.save_dir, f"{args.checkpoint_name}_base_model.pt") )
+  ##################################### Dataset setup ###########################################
+  logger.info(f"calling set_up_datasets")
+  train_set, eval_set = set_up_datasets(tokenizer, seq_len = 128):
+  logger.info(f"saving combined datasets")
+  train_set.save_to_disk(os.path.join(args.save_dir, f"train_dataset")
+  eval_set.save_to_disk(os.path.join(args.save_dir, f"test_dataset"))
+    
 # for preprocessor function and dataset processing
 def set_up_datasets(tokenizer, seq_len = 128):
   en_hu_dataset = load_dataset("tatoeba", lang1="en", lang2="hu") 
@@ -53,6 +109,7 @@ def set_up_datasets(tokenizer, seq_len = 128):
 
   train_dataset = concatenate_datasets(train_splits)
   eval_dataset = concatenate_datasets(test_splits)
+  
   return train_dataset, test_dataset
 
 def preprocess_function(example, seq_len=128, Lang_token_map = lang_tokens):
@@ -74,3 +131,8 @@ def preprocess_function(example, seq_len=128, Lang_token_map = lang_tokens):
   out_items = [input_ids, attention_mask, labels]
 
   return dict(zip(out_cols, out_items))
+
+if __name__ == "__main__" :
+    if version.parse(datasets.__version__) < version.parse("1.18.0"):
+        raise RuntimeError("This script requires Datasets 1.18.0 or higher. Please update via pip install -U datasets.")
+    main()
